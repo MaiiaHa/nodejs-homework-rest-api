2d4d589e5dd2a039d5ba6cfaafb6b6c0e84d7d97
MaiiaHa <maiia.robeiko@ukr.net>
2023-07-25 23:06:50 +0300

    upd server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
 const app = require("./app");
-const { DB_HOST } = require("./config");
-console.log(DB_HOST);
+const { DB_HOST } = process.env;
 
 mongoose.set("strictQuery", true);
 
