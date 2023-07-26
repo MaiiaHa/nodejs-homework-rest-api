9fe46e89aa1918d0b897811fae2d59025ce837ba
MaiiaHa <maiia.robeiko@ukr.net>
2023-07-26 21:23:10 +0300

    upd

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -38,6 +38,10 @@ const updateFavorite = async (req, res, next) => {
   const { id } = req.params;
   const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
 
+  if (!req.body) {
+    throw HttpError(400, "Missing field favorite");
+  }
+
   if (!result) {
     throw HttpError(404, "Not found");
   }
